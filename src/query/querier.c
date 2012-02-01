45e96ad213f1bfc8216e301dcbf720ced56de4d5
lxndr <alexandrburobin@gmail.com>
2012-02-01 17:02:05 +0400

    Add functions to get the socket's local port

--- src/query/querier.c
+++ src/query/querier.c
@@ -671,3 +671,18 @@ gsq_socket_recveived (GSocket *socket, GIOCondition condition, gpointer udata)
 	
 	return TRUE;
 }
+
+
+guint16
+gsq_querier_get_ipv4_socket_port ()
+{
+	GSocketAddress *addr = g_socket_get_local_address (ip4sock, NULL);
+	return g_inet_socket_address_get_port (G_INET_SOCKET_ADDRESS (addr));
+}
+
+guint16
+gsq_querier_get_ipv6_socket_port ()
+{
+	GSocketAddress *addr = g_socket_get_local_address (ip6sock, NULL);
+	return g_inet_socket_address_get_port (G_INET_SOCKET_ADDRESS (addr));
+}
