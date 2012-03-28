b24cc6d5c668a72c4214cbfade30053d9bf6e65d
lxndr <lxndr87i@gmail.com>
2012-03-28 23:31:35 +0400

    Disable controls if no server is selected

--- src/gui-chat.h
+++ src/gui-chat.h
@@ -32,7 +32,7 @@
 GtkWidget *gui_chat_create ();
 void gui_chat_init (GsClient *client);
 void gui_chat_free (GsClient *client);
-void gui_chat_set (GsClient *client);
+void gui_chat_setup (GsClient *client);
 void gui_chat_send (GsClient *client, const gchar *cmd);
 void gui_chat_log (GsClient *client, const gchar *name, gint team, const gchar *msg);
 
