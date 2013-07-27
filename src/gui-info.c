339b78a30325462a3bd5823ef997385dc622ab40
lxndr <lxndr87i@gmail.com>
2013-07-27 19:53:19 +0400

    port Info panel to GResource and GtkBuilder

--- src/gui-info.c
+++ src/gui-info.c
@@ -31,12 +31,12 @@
 
 
 
-static GtkWidget *grid, *ctl_address, *ctl_name, *ctl_game, *ctl_map,
-		*ctl_players, *ctl_password, *ctl_version, *ctl_location, *ctl_message;
-static GtkWidget *toolbar, *ctl_remove, *ctl_favorite, *ctl_connect;
+static GObject *grid, *ctl_address, *ctl_name, *ctl_game, *ctl_map,
+		*ctl_players, *ctl_private, *ctl_version, *ctl_location, *ctl_message;
+static GObject *toolbar, *ctl_remove, *ctl_favorite, *ctl_connect;
 
 
-static void favorite_toggled (GtkToggleButton *button, gpointer udata);
+void gui_info_favorite_toggled (GtkToggleButton *button, gpointer udata);
 
 
 
@@ -45,15 +45,15 @@ gui_info_setup (GsClient *cl)
 {
 	if (cl) {
 		/* enable and set up the toolbar */
-		gtk_widget_set_sensitive (toolbar, TRUE);
+		gtk_widget_set_sensitive (GTK_WIDGET (toolbar), TRUE);
 		
-		g_signal_handlers_block_by_func (ctl_favorite, favorite_toggled, NULL);
+		g_signal_handlers_block_by_func (ctl_favorite, gui_info_favorite_toggled, NULL);
 		gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (ctl_favorite),
 				gs_client_get_favorite (cl));
-		g_signal_handlers_unblock_by_func (ctl_favorite, favorite_toggled, NULL);
+		g_signal_handlers_unblock_by_func (ctl_favorite, gui_info_favorite_toggled, NULL);
 		
 		/* enable and set up the information */
-		gtk_widget_set_sensitive (grid, TRUE);
+		gtk_widget_set_sensitive (GTK_WIDGET (grid), TRUE);
 		
 		gtk_label_set_text (GTK_LABEL (ctl_address), gs_client_get_address (cl));
 		gtk_label_set_text (GTK_LABEL (ctl_location), cl->country);
@@ -65,16 +65,16 @@ gui_info_setup (GsClient *cl)
 						_("This server is not responding and most likely offline."));
 			else
 				gtk_label_set_text (GTK_LABEL (ctl_message), cl->error->message);
-			gtk_widget_set_visible (ctl_message, TRUE);
+			gtk_widget_set_visible (GTK_WIDGET (ctl_message), TRUE);
 		} else {
-			gtk_widget_set_visible (ctl_message, FALSE);
+			gtk_widget_set_visible (GTK_WIDGET (ctl_message), FALSE);
 			gtk_label_set_text (GTK_LABEL (ctl_message), NULL);
 		}
 	} else {
 		/* none is selected. disable the toolbar and clear information grid */
-		gtk_widget_set_sensitive (toolbar, FALSE);
-		gtk_widget_set_sensitive (grid, FALSE);
-		gtk_widget_set_visible (ctl_message, FALSE);
+		gtk_widget_set_sensitive (GTK_WIDGET (toolbar), FALSE);
+		gtk_widget_set_sensitive (GTK_WIDGET (grid), FALSE);
+		gtk_widget_set_visible (GTK_WIDGET (ctl_message), FALSE);
 		gtk_label_set_text (GTK_LABEL (ctl_message), NULL);
 		
 		gtk_label_set_text (GTK_LABEL (ctl_address), _("N/A"));
@@ -82,7 +82,7 @@ gui_info_setup (GsClient *cl)
 		gtk_label_set_text (GTK_LABEL (ctl_game), _("N/A"));
 		gtk_label_set_text (GTK_LABEL (ctl_map), _("N/A"));
 		gtk_label_set_text (GTK_LABEL (ctl_players), _("N/A"));
-		gtk_label_set_text (GTK_LABEL (ctl_password), _("N/A"));
+		gtk_label_set_text (GTK_LABEL (ctl_private), _("N/A"));
 		gtk_label_set_text (GTK_LABEL (ctl_version), _("N/A"));
 		gtk_label_set_text (GTK_LABEL (ctl_location), _("N/A"));
 	}
@@ -101,12 +101,12 @@ gui_info_update (GsClient *client)
 	
 	const gchar *mapname = client->querier->map->str;
 	
-	gtk_widget_set_visible (ctl_message, FALSE);
+	gtk_widget_set_visible (GTK_WIDGET (ctl_message), FALSE);
 	gtk_label_set_text (GTK_LABEL (ctl_name), client->querier->name->str);
 	gtk_label_set_text (GTK_LABEL (ctl_game), *gamename ? gamename : _("N/A"));
 	gtk_label_set_text (GTK_LABEL (ctl_map), *mapname ? mapname : _("N/A"));
 	gtk_label_set_text (GTK_LABEL (ctl_players), players);
-	gtk_label_set_text (GTK_LABEL (ctl_password), client->password ? _("Yes") : _("No"));
+	gtk_label_set_text (GTK_LABEL (ctl_private), client->password ? _("Yes") : _("No"));
 	gtk_label_set_text (GTK_LABEL (ctl_version), client->version);
 	
 	g_free (players);
@@ -118,153 +118,41 @@ gui_info_update (GsClient *client)
 GtkWidget *
 gui_info_create ()
 {
-	GtkWidget *label;
+	GError *error = NULL;
+	GtkBuilder *builder = gtk_builder_new ();
+	if (gtk_builder_add_from_resource (builder, "/org/gswatcher/ui/info.ui", &error) == 0) {
+		g_warning (error->message);
+		g_object_unref (builder);
+		g_error_free (error);
+		return NULL;
+	}
 	
-	grid = gtk_grid_new ();
-	g_object_set (G_OBJECT (grid),
-			"column-spacing", 8,
-			"row-spacing", 4,
-			"border-width", 4,
-			"sensitive", FALSE,
-			NULL);
+	grid         = gtk_builder_get_object (builder, "panel");
+	ctl_name     = gtk_builder_get_object (builder, "name");
+	ctl_address  = gtk_builder_get_object (builder, "address");
+	ctl_game     = gtk_builder_get_object (builder, "game");
+	ctl_map      = gtk_builder_get_object (builder, "map");
+	ctl_players  = gtk_builder_get_object (builder, "players");
+	ctl_private  = gtk_builder_get_object (builder, "private");
+	ctl_version  = gtk_builder_get_object (builder, "version");
+	ctl_location = gtk_builder_get_object (builder, "location");
+	ctl_message  = gtk_builder_get_object (builder, "message");
 	
-	label = gtk_label_new (_("Name:"));
-	g_object_set (G_OBJECT (label),
-			"xalign", 0.0f,
-			"yalign", 0.0f,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), label, 0, 0, 1, 1);
-	
-	ctl_name = gtk_label_new (NULL);
-	g_object_set (G_OBJECT (ctl_name),
-			"xalign", 0.0f,
-			"yalign", 0.0f,
-			"hexpand", TRUE,
-			"wrap", TRUE,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), ctl_name, 1, 0, 1, 1);
-	
-	label = gtk_label_new (_("Address:"));
-	g_object_set (G_OBJECT (label),
-			"xalign", 0.0f,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), label, 0, 1, 1, 1);
-	
-	ctl_address = gtk_label_new (NULL);
-	g_object_set (G_OBJECT (ctl_address),
-			"xalign", 0.0f,
-			"hexpand", TRUE,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), ctl_address, 1, 1, 1, 1);
-	
-	label = gtk_label_new (_("Game:"));
-	g_object_set (G_OBJECT (label),
-			"xalign", 0.0f,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), label, 0, 2, 1, 1);
-	
-	ctl_game = gtk_label_new (NULL);
-	g_object_set (G_OBJECT (ctl_game),
-			"xalign", 0.0f,
-			"hexpand", TRUE,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), ctl_game, 1, 2, 1, 1);
-	
-	label = gtk_label_new (_("Map:"));
-	g_object_set (G_OBJECT (label),
-			"xalign", 0.0f,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), label, 0, 3, 1, 1);
-	
-	ctl_map = gtk_label_new (NULL);
-	g_object_set (G_OBJECT (ctl_map),
-			"xalign", 0.0f,
-			"hexpand", TRUE,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), ctl_map, 1, 3, 1, 1);
-	
-	label = gtk_label_new (_("Players:"));
-	g_object_set (G_OBJECT (label),
-			"xalign", 0.0f,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), label, 0, 4, 1, 1);
-	
-	ctl_players = gtk_label_new (NULL);
-	g_object_set (G_OBJECT (ctl_players),
-			"xalign", 0.0f,
-			"hexpand", TRUE,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), ctl_players, 1, 4, 1, 1);
-	
-	label = gtk_label_new (_("Password:"));
-	g_object_set (G_OBJECT (label),
-			"xalign", 0.0f,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), label, 0, 5, 1, 1);
-	
-	ctl_password = gtk_label_new (NULL);
-	g_object_set (G_OBJECT (ctl_password),
-			"xalign", 0.0f,
-			"hexpand", TRUE,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), ctl_password, 1, 5, 1, 1);
-	
-	label = gtk_label_new (_("Version:"));
-	g_object_set (G_OBJECT (label),
-			"xalign", 0.0f,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), label, 0, 6, 1, 1);
-	
-	ctl_version = gtk_label_new (NULL);
-	g_object_set (G_OBJECT (ctl_version),
-			"xalign", 0.0f,
-			"hexpand", TRUE,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), ctl_version, 1, 6, 1, 1);
-	
-	label = gtk_label_new (_("Location:"));
-	g_object_set (G_OBJECT (label),
-			"xalign", 0.0f,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), label, 0, 7, 1, 1);
-	
-	ctl_location = gtk_label_new (NULL);
-	g_object_set (G_OBJECT (ctl_location),
-			"xalign", 0.0f,
-			"hexpand", TRUE,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), ctl_location, 1, 7, 1, 1);
-	
-	gtk_widget_show_all (grid);
-	
-	PangoAttrList *attrs = pango_attr_list_new ();
-	PangoAttribute *attr = pango_attr_foreground_new (65535, 0, 0);
-	pango_attr_list_insert (attrs, attr);
-	attr = pango_attr_style_new (PANGO_STYLE_ITALIC);
-	pango_attr_list_insert (attrs, attr);
-	
-	ctl_message = gtk_label_new (NULL);
-	g_object_set (G_OBJECT (ctl_message),
-			"visible", FALSE,
-			"attributes", attrs,
-			"hexpand", TRUE,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), ctl_message, 1, 8, 2, 1);
-	pango_attr_list_unref (attrs);
-	
-	return grid;
+	grid = g_object_ref (grid);
+	g_object_unref (builder);
+	return GTK_WIDGET (grid);
 }
 
 
-static void
-remove_clicked (GtkButton *button, gpointer udata)
+void G_MODULE_EXPORT
+gui_info_remove_clicked (GtkButton *button, gpointer udata)
 {
 	GsClient *client = gui_slist_get_selected ();
 	gs_application_remove_server_ask (app, client);
 }
 
-static void
-favorite_toggled (GtkToggleButton *button, gpointer udata)
+void G_MODULE_EXPORT
+gui_info_favorite_toggled (GtkToggleButton *button, gpointer udata)
 {
 	GsClient *client = gui_slist_get_selected ();
 	gboolean favorite = gtk_toggle_button_get_active (button);
@@ -273,8 +161,8 @@ favorite_toggled (GtkToggleButton *button, gpointer udata)
 	gs_application_save_server_list (app);
 }
 
-static void
-connect_clicked (GtkButton *button, gpointer udata)
+void G_MODULE_EXPORT
+gui_info_connect_clicked (GtkButton *button, gpointer udata)
 {
 	GsClient *client = gui_slist_get_selected ();
 	gs_client_connect_to_game (client);
@@ -284,45 +172,31 @@ connect_clicked (GtkButton *button, gpointer udata)
 GtkWidget *
 gui_info_create_bar ()
 {
-	GtkWidget *image;
+	GError *error = NULL;
+	GtkBuilder *builder = gtk_builder_new ();
+	if (gtk_builder_add_from_resource (builder, "/org/gswatcher/ui/info.ui", &error) == 0) {
+		g_warning (error->message);
+		g_object_unref (builder);
+		g_error_free (error);
+		return NULL;
+	}
 	
-	image = gtk_image_new_from_stock (GTK_STOCK_REMOVE, GTK_ICON_SIZE_MENU);
-	ctl_remove = gtk_button_new ();
-	g_object_set (G_OBJECT (ctl_remove),
-			"tooltip-text", _("Remove selected server"),
-			"image", image,
-			NULL);
-	g_signal_connect (ctl_remove, "clicked", G_CALLBACK (remove_clicked), NULL);
+	toolbar      = gtk_builder_get_object (builder, "toolbar");
+	ctl_remove   = gtk_builder_get_object (builder, "remove");
+	ctl_favorite = gtk_builder_get_object (builder, "favorite");
+	ctl_connect  = gtk_builder_get_object (builder, "connect");
 	
-	image = gtk_image_new_from_icon_name ("emblem-favorite", GTK_ICON_SIZE_MENU);
-	ctl_favorite = gtk_toggle_button_new ();
-	g_object_set (G_OBJECT (ctl_favorite),
-			"tooltip-text", _("Favor selected server"),
-			"image", image,
-			NULL);
-	g_signal_connect (ctl_favorite, "toggled", G_CALLBACK (favorite_toggled), NULL);
-	
-	image = gtk_image_new_from_stock (GTK_STOCK_MEDIA_PLAY, GTK_ICON_SIZE_MENU);
-	ctl_connect = gtk_button_new_with_label (_("Connect"));
-	g_object_set (G_OBJECT (ctl_connect),
-			"tooltip-text", _("Connect to selected server"),
-			"image", image,
-			NULL);
-	g_signal_connect (ctl_connect, "clicked", G_CALLBACK (connect_clicked), NULL);
+	gtk_builder_connect_signals (builder, NULL);
+	toolbar = g_object_ref (toolbar);
 	
 	GtkWidget *sep = gtk_separator_new (GTK_ORIENTATION_VERTICAL);
-	
 	GtkWidget *add = gui_plist_create_bar ();
-	
-	toolbar = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 4);
-	g_object_set (G_OBJECT (toolbar),
-			"sensitive", FALSE,
-			NULL);
-	gtk_box_pack_start (GTK_BOX (toolbar), ctl_remove, FALSE, TRUE, 0);
-	gtk_box_pack_start (GTK_BOX (toolbar), ctl_favorite, FALSE, TRUE, 0);
-	gtk_box_pack_start (GTK_BOX (toolbar), ctl_connect, FALSE, TRUE, 0);
 	gtk_box_pack_start (GTK_BOX (toolbar), sep, FALSE, TRUE, 0);
+	gtk_widget_show (sep);
 	gtk_box_pack_start (GTK_BOX (toolbar), add, FALSE, TRUE, 0);
-	gtk_widget_show_all (toolbar);
-	return toolbar;
+	gtk_widget_show (add);
+	
+	gui_info_setup (NULL);
+	
+	return GTK_WIDGET (toolbar);
 }
