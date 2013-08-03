afcfe9c6d36ef032c881390b00e4861002755369
lxndr <lxndr87i@gmail.com>
2013-08-03 15:08:29 +0400

    port Preferences to GResource and GtkBuilder

--- src/gui-preferences.c
+++ src/gui-preferences.c
@@ -31,12 +31,12 @@
 #include "platform.h"
 
 
-static GtkAdjustment *ctl_interval, *ctl_port;
-static GtkWidget *ctl_game_column, *ctl_nenable, *ctl_nsound, *ctl_sysfont,
+static GObject *ctl_interval, *ctl_port;
+static GObject *ctl_game_column, *ctl_nenable, *ctl_nsound, *ctl_sysfont,
 		*ctl_fontlabel, *ctl_font, *ctl_logaddress, *ctl_connect;
 
 
-static void
+void
 gui_prefs_interval_changed (GtkAdjustment *adjustment, gpointer udata)
 {
 	gdouble value = gtk_adjustment_get_value (adjustment);
@@ -45,7 +45,7 @@ gui_prefs_interval_changed (GtkAdjustment *adjustment, gpointer udata)
 }
 
 
-static void
+void
 gui_prefs_gamecolumn_changed (GtkComboBox *widget, gpointer udata)
 {
 	gui_slist_set_game_column_mode (gtk_combo_box_get_active (widget));
@@ -53,7 +53,7 @@ gui_prefs_gamecolumn_changed (GtkComboBox *widget, gpointer udata)
 }
 
 
-static void
+void
 gui_prefs_port_changed (GtkAdjustment *adjustment, gpointer udata)
 {
 	guint16 value = (guint16) gtk_adjustment_get_value (adjustment);
@@ -62,7 +62,7 @@ gui_prefs_port_changed (GtkAdjustment *adjustment, gpointer udata)
 }
 
 
-static void
+void
 gui_prefs_enable_toggled (GtkToggleButton *togglebutton, gpointer udata)
 {
 	gboolean value = gtk_toggle_button_get_active (togglebutton);
@@ -71,8 +71,8 @@ gui_prefs_enable_toggled (GtkToggleButton *togglebutton, gpointer udata)
 }
 
 
-static void
-gs_prefs_sound_changed (GtkFileChooserButton *widget, gpointer udata)
+void
+gui_prefs_sound_changed (GtkFileChooserButton *widget, gpointer udata)
 {
 	gchar *fname = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (widget));
 	gs_notification_set_sound (fname);
@@ -81,25 +81,25 @@ gs_prefs_sound_changed (GtkFileChooserButton *widget, gpointer udata)
 }
 
 
-static void
-gs_prefs_sound_test_clicked (GtkButton *button, gpointer udata)
+void
+gui_prefs_sound_test_clicked (GtkButton *button, gpointer udata)
 {
 	gs_notification_sound ();
 }
 
 
-static void
+void
 gui_prefs_sysfont_toggled (GtkToggleButton *togglebutton, gpointer udata)
 {
 	gboolean value = gtk_toggle_button_get_active (togglebutton);
-	gtk_widget_set_sensitive (ctl_font, !value);
-	gtk_widget_set_sensitive (ctl_fontlabel, !value);
+	gtk_widget_set_sensitive (GTK_WIDGET (ctl_font), !value);
+	gtk_widget_set_sensitive (GTK_WIDGET (ctl_fontlabel), !value);
 	gui_console_set_use_system_font (value);
 	gs_application_save_preferences ();
 }
 
 
-static void
+void
 gui_prefs_font_changed (GtkFontButton *widget, gpointer udata)
 {
 	gchar *fontname = gtk_font_chooser_get_font (GTK_FONT_CHOOSER (ctl_font));
@@ -109,7 +109,7 @@ gui_prefs_font_changed (GtkFontButton *widget, gpointer udata)
 }
 
 
-static void
+void
 gui_prefs_logaddress_changed (GtkEntry *entry, gpointer user_data)
 {
 	const gchar *address = gtk_entry_get_text (entry);
@@ -118,7 +118,7 @@ gui_prefs_logaddress_changed (GtkEntry *entry, gpointer user_data)
 }
 
 
-static void
+void
 gui_prefs_connect_changed (GtkEntry *entry, gpointer user_data)
 {
 	const gchar *command = gtk_entry_get_text (entry);
@@ -126,224 +126,35 @@ gui_prefs_connect_changed (GtkEntry *entry, gpointer user_data)
 	gs_application_save_preferences ();
 }
 
-														
+
 GtkWidget *
 gui_prefs_create ()
 {
-	GtkWidget *box, *label, *tmp;
+	GObject *root = NULL;
+	GError *error = NULL;
+	GtkBuilder *builder = gtk_builder_new ();
+	if (gtk_builder_add_from_resource (builder, "/org/gswatcher/ui/preferences.ui", &error) == 0) {
+		g_warning (error->message);
+		g_object_unref (builder);
+		g_error_free (error);
+		return NULL;
+	}
 	
-	PangoAttrList *bold = pango_attr_list_new ();
-	PangoAttribute *attr = pango_attr_weight_new (PANGO_WEIGHT_BOLD);
-	pango_attr_list_insert (bold, attr);
+	root               = gtk_builder_get_object (builder, "root");
+	ctl_interval       = gtk_builder_get_object (builder, "interval");
+	ctl_game_column    = gtk_builder_get_object (builder, "game-column");
+	ctl_port           = gtk_builder_get_object (builder, "port");
+	ctl_nenable        = gtk_builder_get_object (builder, "notification-enable");
+	ctl_nsound         = gtk_builder_get_object (builder, "notification-sound");
+	ctl_sysfont        = gtk_builder_get_object (builder, "system-font");
+	ctl_fontlabel      = gtk_builder_get_object (builder, "font-label");
+	ctl_font           = gtk_builder_get_object (builder, "font");
+	ctl_logaddress     = gtk_builder_get_object (builder, "log-address");
+	ctl_connect        = gtk_builder_get_object (builder, "connect-command");
 	
-	GtkWidget *grid = gtk_grid_new ();
-	g_object_set (G_OBJECT (grid),
-			"column-spacing", 8,
-			"row-spacing",	2,
-			"border-width", 8,
-			NULL);
-	
-	// General section
-	label = gtk_label_new (_("General"));
-	gtk_label_set_attributes (GTK_LABEL (label), bold);
-	
-	tmp = gtk_separator_new (GTK_ORIENTATION_HORIZONTAL);
-	
-	box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 8);
-	gtk_box_pack_start (GTK_BOX (box), label, FALSE, TRUE, 0);
-	gtk_box_pack_start (GTK_BOX (box), tmp, TRUE, TRUE, 0);
-	gtk_grid_attach (GTK_GRID (grid), box, 0, 0, 2, 1);
-	
-	// Update interval
-	GtkWidget *spin = gtk_spin_button_new_with_range (0.5, 15.0, 0.5);
-	g_object_set (G_OBJECT (spin),
-			"hexpand", TRUE,
-			NULL);
-	ctl_interval = gtk_spin_button_get_adjustment (GTK_SPIN_BUTTON (spin));
-	g_signal_connect (ctl_interval, "value-changed",
-			G_CALLBACK (gui_prefs_interval_changed), NULL);
-	gtk_grid_attach (GTK_GRID (grid), spin, 1, 1, 1, 1);
-	
-	label = gtk_label_new (_("Update _interval:"));
-	g_object_set (G_OBJECT (label),
-			"use-underline", TRUE,
-			"mnemonic-widget", spin,
-			"margin-left", 8,
-			"halign", GTK_ALIGN_START,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), label, 0, 1, 1, 1);
-	
-	// Game column mode
-	ctl_game_column = gtk_combo_box_text_new ();
-	gtk_combo_box_text_append (GTK_COMBO_BOX_TEXT (ctl_game_column),
-			"icon-only", _("Icon only"));
-	gtk_combo_box_text_append (GTK_COMBO_BOX_TEXT (ctl_game_column),
-			"short-text", _("Game name"));
-	gtk_combo_box_text_append (GTK_COMBO_BOX_TEXT (ctl_game_column),
-			"full-text", _("Game name and game mode if known"));
-	gtk_combo_box_set_active (GTK_COMBO_BOX (ctl_game_column), 0);
-	g_signal_connect (ctl_game_column, "changed",
-			G_CALLBACK (gui_prefs_gamecolumn_changed), NULL);
-	gtk_grid_attach (GTK_GRID (grid), ctl_game_column, 1, 2, 1, 1);
-	
-	label = gtk_label_new (_("_Game column:"));
-	g_object_set (G_OBJECT (label),
-			"use-underline", TRUE,
-			"mnemonic-widget", ctl_game_column,
-			"margin-left", 8,
-			"halign", GTK_ALIGN_START,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), label, 0, 2, 1, 1);
-	
-	/* Port */
-	spin = gtk_spin_button_new_with_range (0.0, 65536.0, 1.0);
-	g_object_set (G_OBJECT (spin),
-			"hexpand", TRUE,
-			"numeric", TRUE,
-			"digits", 0,
-			NULL);
-	ctl_port = gtk_spin_button_get_adjustment (GTK_SPIN_BUTTON (spin));
-	g_signal_connect (ctl_port, "value-changed",
-			G_CALLBACK (gui_prefs_port_changed), NULL);
-	gtk_grid_attach (GTK_GRID (grid), spin, 1, 3, 1, 1);
-	
-	label = gtk_label_new (_("Port:"));
-	g_object_set (G_OBJECT (label),
-			"use-underline", TRUE,
-			"mnemonic-widget", spin,
-			"margin-left", 8,
-			"halign", GTK_ALIGN_START,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), label, 0, 3, 1, 1);
-	
-	/* Connect command */
-	ctl_connect = gtk_entry_new ();
-	g_object_set (G_OBJECT (ctl_connect),
-			"hexpand", TRUE,
-			NULL);
-	g_signal_connect (ctl_connect, "changed",
-			G_CALLBACK (gui_prefs_connect_changed), NULL);
-	gtk_grid_attach (GTK_GRID (grid), ctl_connect, 1, 4, 1, 1);
-	
-	label = gtk_label_new (_("Connect command:"));
-	g_object_set (G_OBJECT (label),
-			"use-underline", TRUE,
-			"mnemonic-widget", ctl_connect,
-			"margin-left", 8,
-			"halign", GTK_ALIGN_START,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), label, 0, 4, 1, 1);
-	
-	// Notifications section
-	label = gtk_label_new (_("Notifications"));
-	gtk_label_set_attributes (GTK_LABEL (label), bold);
-	
-	tmp = gtk_separator_new (GTK_ORIENTATION_HORIZONTAL);
-	
-	box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 8);
-	g_object_set (G_OBJECT (box),
-			"margin-top", 6,
-			NULL);
-	gtk_box_pack_start (GTK_BOX (box), label, FALSE, TRUE, 0);
-	gtk_box_pack_start (GTK_BOX (box), tmp, TRUE, TRUE, 0);
-	gtk_grid_attach (GTK_GRID (grid), box, 0, 5, 2, 1);
-	
-	// Enable
-	ctl_nenable = gtk_check_button_new_with_label (_("E_nable"));
-	g_object_set (G_OBJECT (ctl_nenable),
-			"use-underline", TRUE,
-			"margin-left", 8,
-			NULL);
-	g_signal_connect (ctl_nenable, "toggled", G_CALLBACK (gui_prefs_enable_toggled), NULL);
-	gtk_grid_attach (GTK_GRID (grid), ctl_nenable, 0, 6, 2, 1);
-	
-	// Sound
-	ctl_nsound = gtk_file_chooser_button_new (_("Choose notification sound"),
-			GTK_FILE_CHOOSER_ACTION_OPEN);
-	g_object_set (G_OBJECT (ctl_nsound),
-			"hexpand", TRUE,
-			NULL);
-	g_signal_connect (ctl_nsound, "file-set", G_CALLBACK (gs_prefs_sound_changed), NULL);
-	
-	GtkWidget *test_button = gtk_button_new_from_stock (GTK_STOCK_MEDIA_PLAY);
-	g_signal_connect (test_button, "clicked",
-			G_CALLBACK (gs_prefs_sound_test_clicked), NULL);
-	
-	GtkWidget *sound_box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 4);
-	gtk_box_pack_start (GTK_BOX (sound_box), ctl_nsound, TRUE, TRUE, 0);
-	gtk_box_pack_start (GTK_BOX (sound_box), test_button, FALSE, TRUE, 0);
-	gtk_grid_attach (GTK_GRID (grid), sound_box, 1, 7, 1, 1);
-	
-	label = gtk_label_new (_("_Sound:"));
-	g_object_set (G_OBJECT (label),
-			"use-underline", TRUE,
-			"mnemonic-widget", ctl_nsound,
-			"margin-left", 8,
-			"halign", GTK_ALIGN_START,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), label, 0, 7, 1, 1);
-	
-	// Console section
-	label = gtk_label_new (_("Console"));
-	gtk_label_set_attributes (GTK_LABEL (label), bold);
-	
-	tmp = gtk_separator_new (GTK_ORIENTATION_HORIZONTAL);
-	
-	box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 8);
-	g_object_set (G_OBJECT (box),
-			"margin-top", 6,
-			NULL);
-	gtk_box_pack_start (GTK_BOX (box), label, FALSE, TRUE, 0);
-	gtk_box_pack_start (GTK_BOX (box), tmp, TRUE, TRUE, 0);
-	gtk_grid_attach (GTK_GRID (grid), box, 0, 8, 2, 1);
-	
-	// Use system font
-	ctl_sysfont = gtk_check_button_new_with_label (_("Use s_ystem monospace font"));
-	g_object_set (G_OBJECT (ctl_sysfont),
-			"use-underline", TRUE,
-			"margin-left", 8,
-			NULL);
-	g_signal_connect (ctl_sysfont, "toggled",
-			G_CALLBACK (gui_prefs_sysfont_toggled), NULL);
-	gtk_grid_attach (GTK_GRID (grid), ctl_sysfont, 0, 9, 2, 1);
-	
-	// Font
-	ctl_font = gtk_font_button_new_with_font (GS_DEFAULT_MONOSPACE_FONT);
-	g_object_set (G_OBJECT (ctl_font),
-			"hexpand", TRUE,
-			NULL);
-	g_signal_connect (ctl_font, "font-set", G_CALLBACK (gui_prefs_font_changed), NULL);
-	gtk_grid_attach (GTK_GRID (grid), ctl_font, 1, 10, 1, 1);
-	
-	ctl_fontlabel = gtk_label_new (_("_Font:"));
-	g_object_set (G_OBJECT (ctl_fontlabel),
-			"use-underline", TRUE,
-			"mnemonic-widget", ctl_font,
-			"margin-left", 8,
-			"halign", GTK_ALIGN_START,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), ctl_fontlabel, 0, 10, 1, 1);
-	
-	// Log address
-	ctl_logaddress = gtk_entry_new ();
-	g_object_set (G_OBJECT (ctl_logaddress),
-			"hexpand", TRUE,
-			NULL);
-	g_signal_connect (ctl_logaddress, "changed", G_CALLBACK (gui_prefs_logaddress_changed), NULL);
-	gtk_grid_attach (GTK_GRID (grid), ctl_logaddress, 1, 11, 1, 1);
-	
-	label = gtk_label_new (_("_Log address:"));
-	g_object_set (G_OBJECT (label),
-			"use-underline", TRUE,
-			"mnemonic-widget", ctl_logaddress,
-			"margin-left", 8,
-			"halign", GTK_ALIGN_START,
-			NULL);
-	gtk_grid_attach (GTK_GRID (grid), label, 0, 11, 1, 1);
-	
-	pango_attr_list_unref (bold);
-	gtk_widget_show_all (grid);
-	return grid;
+	root = g_object_ref (root);
+	g_object_unref (builder);
+	return GTK_WIDGET (root);
 }
 
 
@@ -351,7 +162,7 @@ void
 gui_prefs_set_interval (gdouble interval)
 {
 	g_signal_handlers_block_by_func (ctl_interval, gui_prefs_interval_changed, NULL);
-	gtk_adjustment_set_value (ctl_interval, interval);
+	gtk_adjustment_set_value (GTK_ADJUSTMENT (ctl_interval), interval);
 	g_signal_handlers_unblock_by_func (ctl_interval, gui_prefs_interval_changed, NULL);
 }
 
@@ -367,7 +178,7 @@ void
 gui_prefs_set_port (guint16 port)
 {
 	g_signal_handlers_block_by_func (ctl_port, gui_prefs_port_changed, NULL);
-	gtk_adjustment_set_value (ctl_port, port);
+	gtk_adjustment_set_value (GTK_ADJUSTMENT (ctl_port), port);
 	g_signal_handlers_unblock_by_func (ctl_port, gui_prefs_port_changed, NULL);
 }
 
@@ -382,9 +193,9 @@ gui_prefs_set_enable_notifications (gboolean enable)
 void
 gui_prefs_set_notification_sound (const gchar *filename)
 {
-	g_signal_handlers_block_by_func (ctl_nsound, gs_prefs_sound_changed, NULL);
+	g_signal_handlers_block_by_func (ctl_nsound, gui_prefs_sound_changed, NULL);
 	gtk_file_chooser_set_filename (GTK_FILE_CHOOSER (ctl_nsound), filename);
-	g_signal_handlers_unblock_by_func (ctl_nsound, gs_prefs_sound_changed, NULL);
+	g_signal_handlers_unblock_by_func (ctl_nsound, gui_prefs_sound_changed, NULL);
 }
 
 void
@@ -392,8 +203,8 @@ gui_prefs_set_use_system_font (gboolean use)
 {
 	g_signal_handlers_block_by_func (ctl_sysfont, gui_prefs_sysfont_toggled, NULL);
 	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (ctl_sysfont), use);
-	gtk_widget_set_sensitive (ctl_font, !use);
-	gtk_widget_set_sensitive (ctl_fontlabel, !use);
+	gtk_widget_set_sensitive (GTK_WIDGET (ctl_font), !use);
+	gtk_widget_set_sensitive (GTK_WIDGET (ctl_fontlabel), !use);
 	g_signal_handlers_unblock_by_func (ctl_sysfont, gui_prefs_sysfont_toggled, NULL);
 }
 
