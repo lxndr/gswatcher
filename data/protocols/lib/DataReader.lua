3c96324c76ffcb2bd58ca35460cd00721f408eae
Alexander Burobin <alexandrburobin@gmail.com>
2025-02-15 22:37:52 +0300

    chore: update year in lua files

--- data/protocols/lib/DataReader.lua
+++ data/protocols/lib/DataReader.lua
@@ -1,5 +1,5 @@
 -- Game Server Watcher
--- Copyright (C) 2023  Alexander Burobin
+-- Copyright (C) 2025  Alexander Burobin
 
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU Affero General Public License as
