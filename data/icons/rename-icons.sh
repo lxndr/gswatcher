ce24122bb2c29dceed8643f5dff99f6b81baab0b
Alexander Burobin <alexandrburobin@gmail.com>
2025-11-02 02:23:21 +0300

    fix(build): hardcode app ID in icon renaming script

--- data/icons/rename-icons.sh
+++ data/icons/rename-icons.sh
@@ -3,7 +3,7 @@
 
 prefix="$1"
 datadir="$2"
-app_id="$3"
+app_id="io.github.lxndr.gswatcher"
 
 for size in 16 24 32 48 64 128 256 512; do
   size_dir="${prefix}/${datadir}/icons/hicolor/${size}x${size}/apps"
